1ddb0fbd49cf3dcb7687637195b2cb6541c557bf
ChungJooEun <nina2439@naver.com>
2021-01-13 18:01:37 +0900

    Spring Data JPA를 이용하여 관계형 데이터 베이스 관리 JPA Auditing을 이용하여 등록/수정 시간 자동화

--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 //Junit5로 넘어와서 바뀐 코드
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -46,6 +47,31 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+
+        //given
+        LocalDateTime now = LocalDateTime.of(2021, 1 ,13, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+            .title("title")
+            .content("content")
+            .author("author")
+            .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate = "+posts.getCreateDate()
+                +", modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
 
     }
 
